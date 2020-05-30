e71c04c4a5126b6638fce9c9715baa229e8f3fd5
flaviostutz <flaviostutz@gmail.com>
2020-05-30 17:51:33 -0300

    updating package name

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/stutzlab/jwt-parser-middleware
+module github.com/stutzlab/gin-jwt-parser
 
 go 1.14
+
+require github.com/gin-gonic/gin v1.6.3
--- jwt-parser.go
+++ jwt-parser.go
@@ -1,6 +1,8 @@
 package jwtparser
 
-"github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 //Config configuration properties for JWT Parser
 type Config struct {
@@ -9,4 +11,6 @@ type Config struct {
 //Middleware Analyses http request, parse existing JWT tokens and set object "jwt" to gin context according to configuration.Middleware
 //The jwt token claims can be later checked by request handlers with "c.GetString(...)"
 func Middleware(config Config) gin.HandlerFunc {
+	return func(context *gin.Context) {
+	}
 }
